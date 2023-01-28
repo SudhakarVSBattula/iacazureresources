257a0e2ab09f65647731669cfb28079d2948ef59
SudhakarVSBattula <sudhakarbvs@outlook.com>
2023-01-28 22:17:29 +0530

    Commiting chages for ARM, Bicep and Terraform again

--- main teraform.tf
+++ main teraform.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "terraform_resource_group" {
-  name     = "terraformresources0914"
+  name     = "terraformresources09145"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "terraform_storage_account" {
-  name                = "storageterraformbvss"
+  name                = "storageterraformbvss1"
   resource_group_name = azurerm_resource_group.terraform_resource_group.name
   location                 = azurerm_resource_group.terraform_resource_group.location
   account_tier             = "Standard"
